6bfd8ed1f15ef16e01faf5a406cbbef62c62f629
android <pavelcep.sust11@gmail.com>
2017-12-12 15:31:45 +0600

    Initial commit

---
+++ app/src/main/java/com/test/in/app/MainActivity.java
@@ -0,0 +1,98 @@
+package com.test.in.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.anjlab.android.iab.v3.BillingProcessor;
+import com.anjlab.android.iab.v3.TransactionDetails;
+
+public class MainActivity extends AppCompatActivity implements BillingProcessor.IBillingHandler {
+
+    BillingProcessor bp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        bp = new BillingProcessor(this, "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvRM/LNQJCDM8BfwrcAPETpVwOqx4Kyj/vUywbCMP7neBglC75aP029MA7H9q5mh5SPH7mP1s5cuyvwnbwSCQubzxnniEt64GlLvYazyzh0loEQQ4wEXPmZUkOQaaanw0KoV3CP9ulNn+L53UkH3jowKBU17XMFLKZ86trCJSnUMbf806f1zUz+I+I3uGKZFv4k0K0xEE+2mTuaUyGMHpoi64LMPFe+rieQH/dsv/6Y7uXnUPu", this);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bp.purchase(MainActivity.this, "android.test.purchased");
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!bp.handleActivityResult(requestCode, resultCode, data)) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+
+    @Override
+    public void onDestroy() {
+        if (bp != null) {
+            bp.release();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onProductPurchased(@NonNull String productId, @Nullable TransactionDetails details) {
+
+    }
+
+    @Override
+    public void onPurchaseHistoryRestored() {
+
+    }
+
+    @Override
+    public void onBillingError(int errorCode, @Nullable Throwable error) {
+        Log.d("parches_error" , ""+errorCode);
+    }
+
+    @Override
+    public void onBillingInitialized() {
+
+    }
+}
